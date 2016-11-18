d7ce1322f5539a96f832c96ce037c20badaab783
manoloIESvirgendelcarmen <manu.chinaski@gmail.com>
2016-11-18 12:26:25 +0100

    Update Repaso.java

--- Repaso.java
+++ Repaso.java
@@ -3,25 +3,22 @@ public class Repaso {
       //creamos un Scanner para leer una palabra
       //almacenamos el contenido en una variable llamada palabraLeida
 
-      //llamada al método devolver en plural
-      //descomentamos la siguiente línea cunado tengamos hecho el método
-      //descomentamos la siguiente línea cunado tengamos hecho el método
+      //descomentamos la siguiente línea cuando tengamos hecho el método
       //String palabraPlural= devolverPalabraPlural(palabraLeida)
 
-      //llamada al método empieza por vocal
       //descomentamos la siguiente línea cunado tengamos hecho el método
       //boolean empiezaVocal = empiezaPorVocal(palabraLeida)
-
-      //llamada al método es una conjunción copulativa
-      //son: Y, E, NI, QUE
+    
       //descomentamos la siguiente línea cunado tengamos hecho el método
       //boolean conjuncioncopulativa= esUnaConjuncionCopulativa(palabraLeida)
 
     }
+    //implementación del método devolverPalabraPlural
+
+    //implementación del método empiezaPorVocal
+
+    //implementación del método  esUnaConjuncionCopulativa
+    //las conjunciones copulativas son:  Y, E, NI, QUE
   }
 
-  //implementación del método devolverPalabraPlural
-
-  //implementación del método empiezaPorVocal
-
-  //implementación del método  esUnaConjuncionCopulativa
+  
