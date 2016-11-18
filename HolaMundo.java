8271d0d42b4fa34e07ad6554fded7d4486c0c1c7
Manuel Molino <manolo@iesvirgendelcarmen.com>
2016-11-18 12:06:37 +0100

    modificado:    .gitignore 	modificado:    HolaMundo.java
    	Se añadió la documentación para javadoc

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,10 @@
+/**Clase para manejo 
+* de git
+* @author Manuel
+* @version 1.0
+*/
 public class HolaMundo {
   public static void main(String[] arg){
     System.out.println("Hola mundo");
-    System.out.println("Nueva línea añadida");
   }
 }
